6dcb49544a6ee1f393f64de93ee87d2dc665d506
mahditrb1999 <mr.trb1378@gmail.com>
2025-05-30 10:27:13 +0330

    first change

---
+++ src/App.jsx
@@ -0,0 +1,14 @@
+import logo from "./logo.svg";
+import Navbar from "./components/navbar/Navbar";
+import Hero from "./components/hero/Hero";
+function App() {
+  return (
+    <div>
+      <Navbar />;
+      <Hero />;
+    </div>
+  );
+}
+
+export default App;
+// export default Navbar;
---
+++ src/components/hero/Hero.jsx
@@ -0,0 +1,11 @@
+import styles from "./hero.module.css";
+function Hero() {
+        return (
+         <div className={styles .ttt}>
+            <h1>dddddddddd</h1>
+            <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Dolorem reiciendis cum repellendus ad dolor deserunt, voluptas laborum quae porro ex harum natus nulla aperiam at est earum eius sunt praesentium.</p>
+         </div>
+        );
+      }
+    
+      export default Hero;
