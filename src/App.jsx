19a14fb22f18781d1a734409962b53e14ea94bca
mahditrb1999 <mr.trb1378@gmail.com>
2025-06-06 10:23:54 +0330

    cd

--- src/App.jsx
+++ src/App.jsx
@@ -5,16 +5,10 @@ import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import Navbar from "./components/navbar/Navbar";
 import Footer from "./components/footer/Footer";
 
-<<<<<<< HEAD
+
 import HomePage from './pages/HomePage';
 import WebDesign from './pages/WebDesign';
 import ContactPage from './pages/ContactPage';
-=======
-
-import ServicesPage from "./pages/ServicesPage";
-import ContactPage from "./pages/ContactPage";
->>>>>>> 3c46fd80dda8ecce5ed679edc9cd70fcc9d20176
-
 import AboutPage from "./pages/AboutPage";
 function App() {
   return (
